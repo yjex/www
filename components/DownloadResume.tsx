c071fce18e316995663eca27802ccc6f09721077
yjex <alexistnlx91@gmail.com>
2024-11-26 00:06:21 +0800

    Fix css issues for mobile

--- components/DownloadResume.tsx
+++ components/DownloadResume.tsx
@@ -7,7 +7,7 @@ export default function DownloadFile () {
       href="/NeoLeXiAlexis_Resume.pdf"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 right-4 p-3 bg-[#60212E] text-white rounded-full hover:bg-[#C49895]"
+      className="fixed bottom-4 right-4 p-3 bg-[#60212E] text-white rounded-full hover:bg-[#C49895] z-20"
     >
         <FileUser className="h-10 w-10" />
     </a>
