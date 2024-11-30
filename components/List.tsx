5ef7807eced7dd706583d16675892d4b48ce5835
yjex <alexistnlx91@gmail.com>
2024-12-01 03:22:37 +0800

    disable any requirement

--- components/List.tsx
+++ components/List.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 "use client"
 import { FunctionComponent } from "react";
 
@@ -11,7 +13,6 @@ export default function List({skillList, title}:{skillList: Skill[], title: stri
         <div className="text-[#60212E] font-bold px-8" style={{fontFamily: "Courier, monospace"}}>
             <h1 className="text-3xl">{title}</h1>
             {skillList.map(({skill, icon: Icon}) => (
-                // @ts-ignore: Ignore type error with icon component
                 <div key={skill} className="flex items-center gap-2 font-serif font-normal text-xl">{skill} <Icon/></div>
             ))}
         </div>
